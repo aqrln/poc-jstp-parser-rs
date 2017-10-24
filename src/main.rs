2120e75c45ac85ba6b8c4d368432a2d2d27294f7
Alexey Orlenko <eaglexrlnk@gmail.com>
2017-10-24 05:18:35 +0300

    check how quote escaping works
    I should really make all of these tests...

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,9 @@ use jstp_parser::*;
 
 fn main() {
     println!(
-        "{:?}\n{:?}",
+        "{:?}\n{:?}\n{:?}",
         parse("[, , null, '', '\\na', 'hai\\n\\\n second \\x0a', true, ]"),
         parse("'nya \\u0439 \\u{1F496}'"),
+        parse("'i love \\'-strings'"),
     );
 }
